9fee5c573b14e560b681226dc49a38dcb6f53bb2
Ivana Dimitrovska <dimitrovska.ivana@interway.com.mk>
2021-03-16 14:18:42 +0100

    Initial commit

---
+++ src/main/java/com/springsecurityexample/demo/auth/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.springsecurityexample.demo.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository){
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if(null==user){
+            throw new UsernameNotFoundException("cannot find username: " + username);
+        }
+        return new LandonUserPrincipal(user);
+    }
+
+}
